8381539e47ff87858ed39aa610da4ce2a255eff3
Hernan Tapia <tapiahernan5@gmail.com>
2025-01-17 20:54:02 -0800

    solution

--- Leet-01/twoSums.js
+++ Leet-01/twoSums.js
@@ -6,4 +6,32 @@ You may assume that each input would have exactly one solution, and you may not 
 
 You can return the answer in any order.
 
+example 1: 
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
 */
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+
+const nums = [2, 7, 11, 15];
+const target = 9;
+
+const twoSums = function(nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        return [i, j];
+      }
+    }
+  }
+  return [];
+};
+
+
+console.log(twoSums(nums, target));
